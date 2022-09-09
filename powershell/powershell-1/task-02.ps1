871e22031292d2e61b7ef27c23afccd166d81a9e
Iina Pirinen <ab7766@student.jamk.fi>
2022-09-09 15:59:33 +0300

    Task-02 ready

---
+++ powershell/powershell-1/task-02.ps1
@@ -0,0 +1,5 @@
+$firstName = Read-Host "Please, give yout first name: "
+$lastName = Read-Host "Please, give yout last name: "
+$groupcode = Read-Host "Please, give yout groupcode: "
+
+Write-Host "Welcome to course, $firstName $lastName, $groupcode"
