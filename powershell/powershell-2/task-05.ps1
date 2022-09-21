eb51658ba7b984669dbd18ce5a5620ff61075266
Iina Pirinen <ab7766@student.jamk.fi>
2022-09-21 17:47:24 +0300

    Tas05 ready

---
+++ powershell/powershell-2/task-05.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$computer = "192.168.1.1",
+    [int]$tries = 1
+)
+
+Write-Host("I will ping $computer $tries, okay?")
+
+$permission = [string](Read-Host("[Y/n]"))
+
+if ($permission -like "Y"){
+    for ($i = 0; $i -lt $tries; $i++)
+    {Write-output("Try " + ($i + 1) + " to ping: " + $computer)}
+}
+elseif ($permission -like "n"){
+    {Permission denied}
+}
+else {
+    {Permission denied}
+}
