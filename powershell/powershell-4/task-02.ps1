6330e66dbdd5fef4876f2d3382509a4f8367498a
Iina Pirinen <ab7766@student.jamk.fi>
2022-10-05 15:55:53 +0300

    Task-02 ready

--- powershell/powershell-4/task-02.ps1
+++ powershell/powershell-4/task-02.ps1
@@ -2,5 +2,5 @@ function Get-FiluInfo2 {
     param (
         [String] $extension = "unknown"
     )
-    Get-ChildItem $HOME -Filter *.txt | Select-Object Name, Length, LastWriteTime
+    Get-ChildItem $HOME -Filter *$extension | Select-Object Name, Length, LastWriteTime
 }
