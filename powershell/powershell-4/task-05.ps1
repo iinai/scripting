85b70c842cf1ac2daa20eba185bc26a847ae1056
Iina Pirinen <ab7766@student.jamk.fi>
2022-10-07 15:20:48 +0300

    Task-05 ready

--- powershell/powershell-4/task-05.ps1
+++ powershell/powershell-4/task-05.ps1
@@ -1,9 +1,16 @@
 param (
-    [String] $foldername
+    [string]$foldername = "unknown"
 )
 
-if (Test-Path -Path $foldername) {
-    Write-Host "Folder $foldername exists!";
+$path = Join-Path $HOME $foldername
+
+if (Test-Path $path) {
+    $files = Get-ChildItem $path | Select-Object Name
+    $filesCount = (Get-ChildItem $path | Measure-Object).Count
+    $newFilePath = Join-Path $HOME files.txt
+    New-Item $newFilePath -ItemType File | Out-Null
+    Add-Content $newFilePath -Value $files
+    Write-Host "$filesCount filenames were written to $newFilePath"
 }
 
 else {Write-Host "Sorry, $foldername does not exist."}
