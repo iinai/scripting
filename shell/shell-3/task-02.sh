52b4efb2bdfb47ed1ec4b1e8471789a1a7c4b67f
Iina Pirinen <ab7766@student.jamk.fi>
2022-11-17 16:49:23 +0200

    task-02 ready

--- shell/shell-3/task-02.sh
+++ shell/shell-3/task-02.sh
@@ -1,5 +1,8 @@
-# Create a script that prints a number of objects in the directory where you executed the script from.
-# Script must consist a function called count_objects. Function must have one local variable, that will hold the actual value, before it's printed to the user.
-# Hints:
-# Function has a print functionality. wc command might be useful.
+#!/bin/bash
 
+function count_objects {
+  COUNT=$(ls $pwd | wc -l)
+  echo "You have $COUNT objects in $(pwd)"
+}
+
+count_objects
