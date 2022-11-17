6b7d4ad0aa063987177f61494447e5ed83c02385
Iina Pirinen <ab7766@student.jamk.fi>
2022-11-17 18:14:35 +0200

    task-05 ready

--- shell/shell-3/task-05.sh
+++ shell/shell-3/task-05.sh
@@ -1,2 +1,52 @@
-# Modify Exercise set 2, Task 5 (Create a script that mimics a simple calculator).
-# Change the logic that all operations are calculated by using functions.
+#!/bin/bash
+
+while true
+do
+    function plus {
+        RESULT=$(($VAR1+$VAR2))
+        echo "Result : ${RESULT}"
+    }
+
+    function miinus {
+        RESULT=$(($VAR1-$VAR2))
+        echo "Result : ${RESULT}"
+    }
+
+    function kerto {
+        RESULT=$(($VAR1*$VAR2))
+        echo "Result : ${RESULT}"
+    }
+
+    function jako {
+        RESULT=$(($VAR1/$VAR2))
+        echo "Result : ${RESULT}"
+    }
+
+    echo "Give operation (+, -, *,/) or type exit to stop:"
+	read OPERATION
+
+	if [ "${OPERATION}" = "exit" ]
+        then
+        	break
+    fi
+
+    echo "Give two numbers:"
+    read VAR1 VAR2
+
+    if [ "${OPERATION}" = "+" ]
+        then
+            plus
+
+        elif [ "${OPERATION}" = "-" ]
+            then
+                miinus
+
+        elif [ "${OPERATION}" = "*" ]
+            then
+                kerto
+
+        elif [ "${OPERATION}" = "/" ]
+            then
+                jako
+    fi
+done
