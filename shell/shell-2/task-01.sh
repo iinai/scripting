33ea88781419cddc9b4383831e83d99e3be1733b
Iina Pirinen <ab7766@student.jamk.fi>
2022-11-23 10:15:05 +0200

    task-01 ready

--- shell/shell-2/task-01.sh
+++ shell/shell-2/task-01.sh
@@ -4,9 +4,9 @@ echo Give the name of the file you want to delete:
 
 read FILENAME
 
-if [[ -f $FILENAME ]]
+if [[ -f $FILENAME && ! -s $FILENAME ]]
 then
-    echo "$FILENAME is a regular file"
+    echo "$FILENAME is a regular file and empty"
     rm $FILENAME
     echo "File $FILENAME deleted"
 else
