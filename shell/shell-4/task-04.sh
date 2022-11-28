bfde8eaf02b2053d04a26ca2c3c223f6e5cc0c0e
Iina Pirinen <ab7766@student.jamk.fi>
2022-11-28 14:33:59 +0200

    task-04 ready

--- shell/shell-4/task-04.sh
+++ shell/shell-4/task-04.sh
@@ -1,7 +1,10 @@
-# Create a script that has a function called network_information that shows the following information:
-# 
-# ip address: 
-# hostname: 
-# 
-# Log the messages in the syslog with a custom tag "network-information".
-# Hints: Utilize arrays and ip or hostname commands with options
+#!/bin/bash
+
+network_information() {
+    NAME=$(hostname)
+    IP=$(hostname -I)
+    echo "${NAME} ${IP}"
+	logger -t "network-information" "${NAME} ${IP}"
+}
+
+network_information
