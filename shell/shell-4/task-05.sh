24140d2b44cfee86dec9fd6f450ee431ea27ebdf
Iina Pirinen <ab7766@student.jamk.fi>
2022-11-28 16:02:17 +0200

    task-05 ready

--- shell/shell-4/task-05.sh
+++ shell/shell-4/task-05.sh
@@ -1,8 +1,25 @@
-# Create a script that has 3 functions: mk_folder, download_image and change_perm.
-# 
-# mk_folder function asks a new folder path from the user and creates the folder.
-# download_image function downloads the following image to the previously created directory and renames it as a linux-tux.png: image
-# 
-# change_perm function sets the folder and image permissions in a way that only owner has rights to them (rwx).
-# 
-# Every possible variable substitution and command execution must be shown in the terminal during the execution.
+#!/bin/bash
+
+set -x
+
+read -p "Give folder path: " FOLDERPATH
+
+mk_folder() {
+    mkdir $FOLDERPATH
+    echo "Folder $FOLDERPATH created"
+}
+
+download_image() {
+    wget https://student.labranet.jamk.fi/~pekkju/script/tux.png -O ${FOLDERPATH}/linux-tux.png
+}
+
+change_perm() {
+    chmod 700 $FOLDERPATH
+    chmod 700 $FOLDERPATH/linux-tux.png
+}
+
+mk_folder
+download_image
+change_perm
+
+set +x
