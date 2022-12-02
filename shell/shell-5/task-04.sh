9802fc399614e71d7097352139b065d61d982e48
Iina Pirinen <ab7766@student.jamk.fi>
2022-12-02 18:07:28 +0200

    task-04 ready

--- shell/shell-5/task-04.sh
+++ shell/shell-5/task-04.sh
@@ -1,5 +1,11 @@
-# Create a script that have a function that changes services listening ports (80 x2) and default page (index.html) defined in example.conf.
-# New values are given as command line arguments during the script startup. Only one port is given and it's used for both fields.
-# NOTE: Script should overwrite the default values, not every possible value in that spot (think that those are values after the apt install command)
-# Hints:
-# Sed command needs some option to write the changes.
+#!/bin/bash
+
+PORT=$1
+PAGE=$2
+
+function change_ports() {
+    sed -i "s/80/$PORT/g" example.conf
+    sed -i "s/index.html/$PAGE/g" example.conf
+}
+
+change_ports
