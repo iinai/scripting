1521852a14b30b4212995c1dc3a7c501a6608ac0
Iina Pirinen <ab7766@student.jamk.fi>
2022-12-02 17:47:44 +0200

    task-03 ready

--- shell/shell-5/task-03.sh
+++ shell/shell-5/task-03.sh
@@ -1,6 +1,30 @@
-# Modify the task 2.
-# Script should have function that adds a new entry to the cars.csv file based on the user input.
-# Every value must be asked from the user separately and value/values added should be given to the function as an argument.
-# Hints:
-# Remember delimiters.
-# One string might be easier than multiple --> how you can combine multiple strings (concatenate)?
+#!/bin/bash
+
+INPUT=$1
+IFS=','
+COUNTER=1
+
+while read MANUFACTURER MODEL COLOR YEAR
+do
+	echo "--- Line $COUNTER ---"
+	echo "Manufacturer: $MANUFACTURER"
+    echo "Model: $MODEL"
+    echo "Color: $COLOR"
+    echo "Year: $YEAR"
+
+    COUNTER=$((COUNTER+1))
+
+done < $INPUT
+
+echo ""
+echo "Add a new car"
+read -p "Manufacturer: " MANUFACTURER
+read -p "Model: " MODEL
+read -p "Color: " COLOR
+read -p "Year: " YEAR
+
+new_entry () {
+    echo ${1},${2},${3},${4} >> cars.csv
+}
+
+new_entry ${MANUFACTURER} ${MODEL} ${COLOR} ${YEAR}
